b050acbdde5035e0e6d011c0e2aeec307a5a135e
ey7 <eoinyoung84@gmail.com>
2019-12-04 22:39:20 +0000

    added code to manage static files during development

--- shopper/urls.py
+++ shopper/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name="index"),
 	path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
