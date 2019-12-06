a38383aba30a0453c2cc8eaa729bbedc54ccd0ef
ey7 <eoinyoung84@gmail.com>
2019-12-06 17:58:51 +0000

    corrected typo in if.form_is_valid function on the register route

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
 	"""
 	if request.method == 'POST':
 		form = UsernameEmailPasswordForm(request.POST)
-		if form_is_valid():
+		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
 			return redirect('login')
