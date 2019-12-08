c064b471a194a1d3272fdb94ccd67c9a06560c25
ey7 <eoinyoung84@gmail.com>
2019-12-08 17:21:53 +0000

    user profile page and python logic added to users app

--- users/forms.py
+++ users/forms.py
@@ -12,6 +12,16 @@ class UsernameEmailPasswordForm(UserCreationForm):
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 
+class EmailUsernameUpdate(forms.ModelForm):
+	"""
+	Form to allow user to update their email and username
+	"""
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ['username', 'email']
+
 
 
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from . forms import UsernameEmailPasswordForm 
+from . forms import UsernameEmailPasswordForm, EmailUsernameUpdate 
 
 def register(request):
 	"""
@@ -18,3 +18,22 @@ def register(request):
 	    form = UsernameEmailPasswordForm()
 
 	return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+	"""
+	Allows user to update their profile information
+	such as username and email
+	"""
+	if request.method == 'POST':
+		form = EmailUsernameUpdate(request.POST,instance=request.user)
+		if form.is_valid():
+			form.save()
+			return redirect('profile')
+
+	else:
+		form = EmailUsernameUpdate(instance=request.user)
+
+	return render(request, 'users/profile.html', {'form': form})
+
+
