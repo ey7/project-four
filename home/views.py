e6eb1ab7d506e5d228afd89248b12b8ca574a546
ey7 <eoinyoung84@gmail.com>
2019-12-09 21:11:27 +0000

    flash success messages added for logout and login actions

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.signals import user_logged_out, user_logged_in
+from django.contrib import messages
 
 def index(request):
 
@@ -7,3 +9,20 @@ def index(request):
 def about(request):
 
 	return render(request, 'home/about.html')
+
+""" credit: https://stackoverflow.com/questions/46542502/django-how-to-add-a-logout-successful
+-message-using-the-django-contrib-auth"""
+
+# show logout message when user logs out
+def show_logout_message(sender, user, request, **kwargs):
+	if user_logged_out:
+		messages.success(request, 'You have been logged out.')
+
+user_logged_out.connect(show_logout_message)
+
+# show login message when user logs in
+def show_login_message(sender, user, request, **kwargs):
+	if user_logged_in:
+		messages.success(request, 'You are logged in.')
+
+user_logged_in.connect(show_login_message)
