ce54f55f7c81f8aac0f1961ef3b5464aa0773700
ey7 <eoinyoung84@gmail.com>
2019-12-12 19:54:07 +0000

    removed image field from product model as a test

--- products/models.py
+++ products/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Product(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField()
-	product_image = models.ImageField()
 	price = models.DecimalField(max_digits=6, decimal_places=2)
 
 	def __str__(self):
