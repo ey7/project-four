33f517bf5cc999988ed1bea68dfe5d65d5dac79c
ey7 <eoinyoung84@gmail.com>
2019-12-12 20:44:22 +0000

    product detail view and template created

--- products/views.py
+++ products/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Product
 from django.views.generic import DetailView, ListView
 
 class ProductListView(ListView):
 	model = Product
+
+class ProductDetailView(DetailView):
+	model = Product
+
+def product_detail_view(request, primary_key):
+    product = get_object_or_404(Product, pk=primary_key)
+    return render(request, 'products/product_detail.html', context={'product': product})
 
 
