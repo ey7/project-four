eca3c0a68ac99877774c90e5aadb5fb85d05867c
ey7 <eoinyoung84@gmail.com>
2019-12-12 21:08:11 +0000

    links added to the individual products on the list view page

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('all-products/', views.ProductListView.as_view(), name='all-products'),
-	path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+	path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
 ]
