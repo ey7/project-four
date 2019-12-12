e6637d0a778d06cf87f2b500bdc86f2ab11c6612
ey7 <eoinyoung84@gmail.com>
2019-12-12 21:42:40 +0000

    removed get object or 404 test from the code

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,
 from . models import Product
 from django.views.generic import DetailView, ListView
 
@@ -7,9 +7,3 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
 	model = Product
-
-def product_detail_view(request, primary_key):
-    product = get_object_or_404(Product, pk=primary_key)
-    return render(request, 'products/product_detail.html', context={'product': product})
-
-
