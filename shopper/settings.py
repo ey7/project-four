7c746f3bd39b5354da3a285ea507d5bf8112ec41
ey7 <eoinyoung84@gmail.com>
2019-12-14 23:09:03 +0000

    changed database settings for django heroku

--- shopper/settings.py
+++ shopper/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'shopper.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 else:
     DATABASES = {
         'default': {
