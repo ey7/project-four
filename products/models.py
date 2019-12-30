24f4dfe0ab5165176c68e1e1bbebf27e8fe8283a
ey7 <eoinyoung84@gmail.com>
2019-12-30 12:03:09 +0000

    added image field to product model

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField()
 	price = models.DecimalField(max_digits=6, decimal_places=2)
+	image = models.ImageField(upload_to='images')
 
 	def __str__(self):
 		return self.title
