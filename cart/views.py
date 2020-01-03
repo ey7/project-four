0ef839fbcf09641b967e68fdf4804f9cf92c3057
ey7 <eoinyoung84@gmail.com>
2020-01-03 23:38:17 +0000

    added quantity variable to cart-view as test

--- cart/views.py
+++ cart/views.py
@@ -18,7 +18,7 @@ def add_to_cart(request, id):
 		cart[id] = cart.get(id, quantity)
 
 		request.session['cart'] = cart
-		return render(request,'cart/cart_view.html',{'quantity':quantity,})
+		return render(request,'cart/cart_view.html')
 
 
 
