645b4d8175607d1bbd3e2b91f8e23b029875fafd
ey7 <eoinyoung84@gmail.com>
2020-01-06 18:54:30 +0000

    name changed from search to icontains in search_results function

--- search/views.py
+++ search/views.py
@@ -7,6 +7,6 @@ def search(request):
 	return render(request, 'search/search.html')
 
 def search_results(request):
-    products = Product.objects.filter(title__search=request.GET.get('q'))
+    products = Product.objects.filter(title__icontains=request.GET.get('q'))
 
     return render(request, "search/search.html", {"products": products})
