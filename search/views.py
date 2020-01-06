666eda7f0ca665d3d59f7f16fa3cb013f2645bc4
ey7 <eoinyoung84@gmail.com>
2020-01-06 20:25:13 +0000

    search function changed as it was not working

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from products.models import Product
+from django.db.models import Q
 
 # Create your views here.
 
@@ -7,6 +8,16 @@ def search(request):
 	return render(request, 'search/search.html')
 
 def search_results(request):
-    products = Product.objects.filter(title__icontains=request.GET.get('q'))
+	""" code: http://www.learningaboutelectronics.com/Articles/
+	How-to-add-search-functionality-to-a-website-in-Django.php"""
+	if request.method == 'GET':
+		query = request.GET.get('q')
 
-    return render(request, "search/search.html", {"products": products})
+		if query is not None:
+			lookups = Q(title__icontains=query) | Q(description__icontains=query)
+
+			results = Product.objects.filter(lookups).distinct()
+
+			return render(request, "search/search.html", {"results": results})
+		else:
+			return render(request, "search/search.html")
