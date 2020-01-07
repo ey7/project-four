47b389147af5134aa8d98cf77432795101c4a991
ey7 <eoinyoung84@gmail.com>
2020-01-07 23:15:22 +0000

    added namespace and appname to products app

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
 	path('', views.ProductListView.as_view(), name='all-products'),
 	path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
--- shopper/urls.py
+++ shopper/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('products/', include('products.urls')),
+    path('products/', include('products.urls', namespace="products")),
     path('search/', include('search.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
