dfeb000b44c62bf78ea37765fa668fc347616541
ey7 <eoinyoung84@gmail.com>
2020-01-09 21:26:19 +0000

    modified slug field in product model unique is true and null is false

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
 	description = models.TextField()
 	price = models.DecimalField(max_digits=6, decimal_places=2)
 	image = models.ImageField(upload_to='images')
-	slug = models.SlugField(null=True)
+	slug = models.SlugField(null=False, unique=True)
 
 	def __str__(self):
 		return self.title
