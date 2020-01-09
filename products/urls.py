a5a904b196b5fba5181a76c35b1a19a46308fc73
ey7 <eoinyoung84@gmail.com>
2020-01-09 20:57:15 +0000

    edited slug url for individual product

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ app_name = 'products'
 
 urlpatterns = [
 	path('', views.ProductListView.as_view(), name='all-products'),
-	path('products/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
+	path('<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
 ]
