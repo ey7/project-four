954078ad77e4aff2f73ea01e2725cfc02e686e5e
ey7 <eoinyoung84@gmail.com>
2020-01-10 22:19:59 +0000

    pagination code added for product list view and pages

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.views.generic import DetailView, ListView
 
 class ProductListView(ListView):
 	model = Product
+	paginate_by = 2
 
 class ProductDetailView(DetailView):
 	model = Product
