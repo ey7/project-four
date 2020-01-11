d9ddc14842a2d5e2f47bbedc926000cbc7fd9e2a
ey7 <eoinyoung84@gmail.com>
2020-01-11 20:27:57 +0000

    forgot to import Category into views.py file

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from . models import Product
+from . models import Product, Category
 from django.views.generic import DetailView, ListView
 
 class ProductListView(ListView):
