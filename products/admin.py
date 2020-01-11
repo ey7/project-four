17d115ced89413e3b0d75d802ece95344a15bb45
ey7 <eoinyoung84@gmail.com>
2020-01-11 17:33:22 +0000

    modifications made to products.admin.py to display category

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from . models import Product
+from . models import Product, Category
+
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ('title',)}
