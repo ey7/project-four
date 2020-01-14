ab5885df2ef80c66c6ff29744f21bf884dea8cc7
ey7 <eoinyoung84@gmail.com>
2020-01-14 21:33:38 +0000

    added category detail view as a list view

--- products/views.py
+++ products/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from . models import Product
 from django.views.generic import DetailView, ListView
 
-def product_category(request, category):
-    products = Product.objects.filter(category=category)
-
-    return render(request, "products/product_list.html", {"products": products})
-
 class ProductListView(ListView):
 	model = Product
 	paginate_by = 2
 
 class ProductDetailView(DetailView):
 	model = Product
+
+class CategoryListView(DetailView):
+	model = Product
+	template_name = 'products/categories.html'
+	queryset = Product.objects.filter(category='Graphite')
 
 
 
