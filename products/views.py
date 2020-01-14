259e22ef0d93a1b04dc458fe3a864f73b7e87b4e
ey7 <eoinyoung84@gmail.com>
2020-01-14 21:00:04 +0000

    moved get category function to top of file

--- products/views.py
+++ products/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from . models import Product
 from django.views.generic import DetailView, ListView
 
+def product_category(request, category):
+    products = Product.objects.filter(category=category)
+
+    return render(request, "products/product_list.html", {"products": products})
+
 class ProductListView(ListView):
 	model = Product
 	paginate_by = 2
@@ -9,8 +14,5 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
 	model = Product
 
-def product_category(request, category):
-    products = Product.objects.filter(category=category)
 
-    return render(request, "products/product_list.html", {"products": products})
 
