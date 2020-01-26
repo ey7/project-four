8ea1e0b8e079d858193c1c8cce3103659c8f686c
ey7 <eoinyoung84@gmail.com>
2020-01-26 18:43:22 +0000

    tests added to the products app

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from . models import Product
 
-# Create your tests here.
+class TestProductsPages(TestCase):
+
+	def test_product_list_template(self):
+		response = self.client.get('/products/')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'products/product_list.html')
+
+	def test_product_detail_template(self):
+		product = Product(
+			category='graphite',
+			title='this is a product title',
+			description='description',
+			price='500',
+			image='graphite.jpg',
+			slug='slug'
+			)
+		product.save()
+
+		response = self.client.get('/products/{}'.format(product.pk))
+		self.assertEqual(response.status_code, 301)
+		
+
+
+
+
+
+
+
+
+
+
