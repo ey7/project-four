7cf06f279bc209f76c60fdb1996f89c1f5b1413f
ey7 <eoinyoung84@gmail.com>
2020-01-26 17:53:15 +0000

    tests added to the users app

--- cart/tests.py
+++ cart/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.test import Client
+from django.shortcuts import reverse
 
 class TestCartPages(TestCase):
 
@@ -8,6 +9,8 @@ class TestCartPages(TestCase):
 	# will check to see if cart view gives 302 redirect as the user must be logged in
 		response = self.client.get('/cart/')
 		self.assertEqual(response.status_code, 302)
+		self.client.post(reverse('cart'))
+
 		response = self.client.get('/carts/')
 		# check to see if the url carts will produce a 404
 		self.assertEqual(response.status_code, 404)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class TestUserPages(TestCase):
+
+	def test_register(self):
+		# will check the register view
+		response = self.client.get('/users/register/')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'users/register.html')
+
+	def test_account_page(self):
+		response = self.client.get('/users/account/')
+		# check that the page will redirect to account as the user needs to be logged in
+		self.assertEqual(response.status_code, 302)
+		self.client.post(reverse('account'))
+		
+
+
+
+
