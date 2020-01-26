1203e90bd7edc05bd07544d6659f987c566f35cd
ey7 <eoinyoung84@gmail.com>
2020-01-26 16:11:51 +0000

    tests for templates in home app added

--- home/tests.py
+++ home/tests.py
@@ -1,8 +1,22 @@
 from django.test import TestCase
 
-class indexPageTest(TestCase):
+class TestHomePages(TestCase):
 
 	def test_homepage_template(self):
 
 		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'index.html')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'home/index.html')
+
+	def test_about_template(self):
+
+		response = self.client.get('/home/about/')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'home/about.html')
+
+	def test_contact_template(self):
+
+		response = self.client.get('/home/contact/')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'home/contact.html')
+
