fbdb011034bbdf4ece01f028ddf598f01d16071b
ey7 <eoinyoung84@gmail.com>
2020-01-26 19:41:35 +0000

    tests added for the chekout app

--- cart/tests.py
+++ cart/tests.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from django.test import Client
 from django.shortcuts import reverse
 
 class TestCartPages(TestCase):
--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+class TestCheckoutPages(TestCase):
+
+	def test_cartview_template(self):
+		# will check to see if checkout view gives 302 redirect as the user must be logged in
+		response = self.client.get('/checkout/')
+		self.assertEqual(response.status_code, 302)
+		self.client.post(reverse('checkout'))
